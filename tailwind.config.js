df747ee143e4c43400bde66d006397e30433feb4
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-09-30 22:41:11 +0200

    feat(slider): create slider with css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
       fontFamily: {
         darker: ['Darker Grotesque'],
       },
-      color: {
+      colors: {
         black: '#000517',
+        grey: '#F7F7F7',
         'primary-base': '#283069',
         'grey-secondary-light': '#F2F0F4',
       },
