bc4240fb83cfca7c6160f9a331feb1e501df859d
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-01 13:58:26 +0200

    feat(slider): add tab

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import './style.css'
 const slider = document.querySelector('#slider')
 const sliderTransitionClass = ['transform', 'duration-300']
 const cards = document.querySelectorAll('#slider li')
+const arrowLeft = document.querySelector('#arrow-left')
+const arrowRight = document.querySelector('#arrow-right')
 const dataset = []
 
 let isMoving = false
@@ -20,7 +22,7 @@ let count = 0
  */
 function translateCards(element, total, movement) {
   total += movement
-  element.style.transform = `translateX(${total}px)`
+  element.style.transform = `translateX(-${total}px)`
   return total
 }
 
@@ -38,12 +40,13 @@ function resetSlider(element, transitionClass, total = 0) {
   return total
 }
 
-cards.forEach((card) => {
+cards.forEach((card, index) => {
   const offsetWidth = card.previousElementSibling ? card.previousElementSibling.offsetWidth : 0
   dataset.push({
     width: card.offsetWidth,
     height: card.offsetHeight,
-    total: count,
+    totalWidth: count,
+    totalMargin: index * 24,
     // Get the next card if 20% if out of the viewport
     withPrevious: count + 0.2 * offsetWidth,
   })
@@ -51,56 +54,119 @@ cards.forEach((card) => {
   count += card.offsetWidth
 })
 
-slider.addEventListener('mousedown', () => {
-  isMoving = true
-  // Remove transition class to avoid animation on mousemove
-  slider.classList.remove(...sliderTransitionClass)
-})
-
-slider.addEventListener('mousemove', (e) => {
-  if (isMoving) {
-    const { movementX } = e
-    totalMovement = translateCards(slider, totalMovement, movementX)
-  }
-})
-
-slider.addEventListener('mouseup', (e) => {
-  isMoving = false
+arrowRight.addEventListener('click', () => {
+  const indexItem = dataset.findIndex(
+    ({ totalWidth, totalMargin }) => totalMovement <= totalMargin + totalWidth
+  )
+  const item = dataset[indexItem + 1]
 
   const lastCard = cards[cards.length - 1]
-  const windowSize = window.innerWidth
-  const posLastCard = lastCard.getBoundingClientRect().right
-  console.log(totalMovement + (windowSize - posLastCard) - 16)
-  if (posLastCard < windowSize) {
-    totalMovement = resetSlider(
-      slider,
-      sliderTransitionClass,
-      // Get distance between last card right and window right
-      totalMovement + (windowSize - posLastCard) - 16
-    )
-  } else if (totalMovement > 0) {
-    totalMovement = resetSlider(slider, sliderTransitionClass)
-  } else {
-    const indexItem = dataset.findIndex(({ withPrevious }) => -totalMovement < withPrevious)
-    const item = dataset[indexItem]
-    const margin = 24 * indexItem
-    console.table(dataset)
-    console.log(item)
+  const leftLastCard = lastCard.getBoundingClientRect().left
+  const widthLastCard = lastCard.getBoundingClientRect().width
+
+  // Last card can't unplug from the right
+  if (leftLastCard < window.innerWidth + widthLastCard) {
+    const lastMovement = leftLastCard - window.innerWidth + widthLastCard + 16
+
     slider.classList.add(...sliderTransitionClass)
-    totalMovement = translateCards(slider, -item.total - margin, 0)
+    totalMovement = translateCards(slider, totalMovement, lastMovement)
+
+    if (arrowRight === document.activeElement) {
+      arrowLeft.focus()
+    }
+    arrowRight.disabled = true
+    arrowLeft.disabled = false
+    return
+  }
+
+  slider.classList.add(...sliderTransitionClass)
+  totalMovement = translateCards(slider, item.totalMargin + item.totalWidth, 0)
+
+  if (totalMovement >= 0) {
+    arrowLeft.disabled = false
   }
 })
 
-document.addEventListener('mouseout', (e) => {
-  const from = e.relatedTarget || e.toElement
-  // Trigger only if mouseout from the page
-  if (!from || from.nodeName === 'HTML') {
-    isMoving = false
-    if (totalMovement > 0) {
-      totalMovement = resetSlider(slider, sliderTransitionClass)
+arrowLeft.addEventListener('click', () => {
+  const indexItem = dataset.findIndex(
+    ({ totalWidth, totalMargin }) => totalMovement <= totalMargin + totalWidth
+  )
+
+  const item = dataset[indexItem - 1]
+  slider.classList.add(...sliderTransitionClass)
+  totalMovement = translateCards(slider, item.totalMargin + item.totalWidth, 0)
+
+  if (totalMovement <= 0) {
+    if (arrowLeft === document.activeElement) {
+      arrowRight.focus()
     }
+    arrowLeft.disabled = true
+    arrowRight.disabled = false
+    return
+  } else {
+    arrowRight.disabled = false
   }
 })
+
+// slider.addEventListener('mousedown', () => {
+//   isMoving = true
+//   // Remove transition class to avoid animation on mousemove
+//   slider.classList.remove(...sliderTransitionClass)
+// })
+
+// slider.addEventListener('mousemove', (e) => {
+//   if (isMoving) {
+//     const { movementX } = e
+//     totalMovement = translateCards(slider, totalMovement, movementX)
+//   }
+// })
+
+// slider.addEventListener('mouseup', (e) => {
+//   isMoving = false
+
+//   const lastCard = cards[cards.length - 1]
+//   const windowSize = window.innerWidth
+//   const posLastCard = lastCard.getBoundingClientRect().right
+//   if (posLastCard < windowSize) {
+//     totalMovement = resetSlider(
+//       slider,
+//       sliderTransitionClass,
+//       // Get distance between last card right and window right
+//       totalMovement + (windowSize - posLastCard) - 16
+//     )
+//   } else if (totalMovement > 0) {
+//     totalMovement = resetSlider(slider, sliderTransitionClass)
+//   } else {
+//     const indexItem = dataset.findIndex(({ withPrevious }) => -totalMovement < withPrevious)
+//     const item = dataset[indexItem]
+//     const margin = 24 * indexItem
+//     console.table(dataset)
+//     console.log(item)
+//     slider.classList.add(...sliderTransitionClass)
+//     totalMovement = translateCards(slider, -item.total - margin, 0)
+//   }
+// })
+
+// document.addEventListener('mouseout', (e) => {
+//   const from = e.relatedTarget || e.toElement
+//   // Trigger only if mouseout from the page
+//   if (!from || from.nodeName === 'HTML') {
+//     isMoving = false
+//     const lastCard = cards[cards.length - 1]
+//     const windowSize = window.innerWidth
+//     const posLastCard = lastCard.getBoundingClientRect().right
+//     if (posLastCard < windowSize) {
+//       totalMovement = resetSlider(
+//         slider,
+//         sliderTransitionClass,
+//         // Get distance between last card right and window right
+//         totalMovement + (windowSize - posLastCard) - 16
+//       )
+//     } else if (totalMovement > 0) {
+//       totalMovement = resetSlider(slider, sliderTransitionClass)
+//     }
+//   }
+// })
 
 // il faut faire la gestion de l'aimantation en utilisant un find dans le mousemove dans le dataset avec le totalMovement
 // il fuat ajouter transform duration-500 lorsuqe il revient à 0 ou lorsqu'il se calera (et commencer à faire du fonctionnel)
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         black: '#000517',
         grey: '#F7F7F7',
         'primary-base': '#283069',
+        'grey-primary-light': '#686984',
         'grey-secondary-light': '#F2F0F4',
       },
     },
